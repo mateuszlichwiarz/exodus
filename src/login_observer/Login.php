6ca12eb3a6c7fdf80b683cac87a0e2d89e2d12f7
Mateusz <mateusz.lichwiarz@gmail.com>
2021-11-30 01:37:15 +0100

    start: login class

---
+++ src/login_observer/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\login_observer;
+
+class Login
+{
+    const LOGIN_UNKNOWN = 1;
+    const LOGIN_WRONG_PASS = 2;
+    const LOGIN_ACCESS = 1;
+    private $status = [];
+
+    public function handleLogin(): bool 
+    {
+        //stuffff
+        // blablabla
+        
+        return true;
+    }
+}
