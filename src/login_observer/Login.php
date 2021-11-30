b50f7d56198d32a0ecaca60b3d6981baf60a6d3c
Mateusz <mateusz.lichwiarz@gmail.com>
2021-12-01 00:11:43 +0100

    random result for user login

--- src/login_observer/Login.php
+++ src/login_observer/Login.php
@@ -13,6 +13,22 @@ class Login
     {
         $isvalid = false;
         
+        switch(rand(1,3))
+        {
+            case 1:
+                $this->setStatus(self::LOGIN_ACCESS, $user, $ip);
+                $isvalid = true;
+                break;
+            case 2:
+                $this->setStatus(self::LOGIN_WRONG_PASS, $user, $ip);
+                $isvalid = false;
+                break;
+            case 3:
+                $this->setStatus(self::LOGIN_UNKNOWN, $user, $ip);
+                $isvalid = false;
+                break;
+        }
+        
         return $isvalid;
     }
 
