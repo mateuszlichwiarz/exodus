1b11472f6a208cb85c899e49a5f0c805c37289fe
Mateusz <mateusz.lichwiarz@gmail.com>
2021-12-01 01:46:52 +0100

    add notify to handleLogin

--- src/login_observer/Login.php
+++ src/login_observer/Login.php
@@ -36,4 +36,36 @@ class Login implements Observable
         }
     }
 
+    public function handleLogin(string $user, string $password, string $ip): bool
+    {
+        $isvalid = false;
+        switch(rand(1, 3))
+        {
+            case 1:
+                $this->setStatus(self::LOGIN_ACCESS, $user, $ip);
+                $isvalid = true;
+                break;
+            case 2:
+                $this->setStatus(self::LOGIN_WRONG_PASS, $user, $ip);
+                $isvalid = false;
+                break;
+            case 3:
+                $this->setStatus(self::LOGIN_USER_UNKNOWN, $user, $ip);
+                $isvalid = false;
+                break;
+        }
+        $this->notify();
+        return $isvalid;
+    }
+
+    private function setStatus(int $status, string $user, string $ip)
+    {
+        $this->status = [$status, $user, $ip];
+    }
+
+    private function getStatus(): array
+    {
+        return $this->status;
+    }
+
 }
