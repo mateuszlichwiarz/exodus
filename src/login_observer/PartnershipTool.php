ab45de0b2e736f6413d1f416dc12a439e9c8cc02
Mateusz <mateusz.lichwiarz@gmail.com>
2021-12-08 02:02:49 +0100

    PartneshipTool class

--- src/login_observer/PartnershipTool.php
+++ src/login_observer/PartnershipTool.php
@@ -4,12 +4,12 @@ namespace App\login_observer;
 
 use App\login_observer\LoginObserver;
 
-class GeneralLogger extends LoginObserver
+class PartnershipTool extends LoginObserver
 {
     public function doUpdate(Login $login)
     {
         $status = $login->getStatus();
         
-        print __CLASS__ . ":    add data to diary";
+        print __CLASS__ . ":    set cookie for allowed ip";
     }
 }
