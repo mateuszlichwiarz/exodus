dccaa99bb90df0a0caa22f3cfbdb7f9a02cebf5f
Mateusz <mateusz.lichwiarz@gmail.com>
2022-12-04 02:37:36 +0100

    dummy Name for users

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,7 +40,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setName('papuszka');
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
