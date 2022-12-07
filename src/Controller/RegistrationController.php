501ee6846e7d861d746250a18e78c24e83c08492
Mateusz <mateusz.lichwiarz@gmail.com>
2022-12-08 00:11:33 +0100

    set redirect to mainpage after authenticate correct

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,7 +45,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('index');
+            return $this->redirectToRoute('mainpage');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -52,7 +52,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('mainpage'));
     }
 
     protected function getLoginUrl(Request $request): string
