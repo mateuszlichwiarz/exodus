58b7ac4f8203a8de830602eaffb44252e520c0de
Mateusz <mateusz.lichwiarz@gmail.com>
2022-12-08 01:04:37 +0100

    temporary mainpage

---
+++ src/Controller/ExodusController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExodusController extends AbstractController
+{
+    /**
+     * @Route("/mainpage", name="mainpage")
+     */
+    public function index()
+    {
+        return new Response('Welcome');
+    }
+}
