9dc278f8e1846949876a8bef0abd3de9acc191fd
Mateusz <mateusz.lichwiarz@gmail.com>
2022-12-18 03:54:23 +0100

    create Weapon entity

---
+++ src/Entity/Weapon.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WeaponRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WeaponRepository::class)
+ */
+class Weapon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dmg;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $req;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDmg(): ?int
+    {
+        return $this->dmg;
+    }
+
+    public function setDmg(int $dmg): self
+    {
+        $this->dmg = $dmg;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getReq(): ?int
+    {
+        return $this->req;
+    }
+
+    public function setReq(int $req): self
+    {
+        $this->req = $req;
+
+        return $this;
+    }
+}
