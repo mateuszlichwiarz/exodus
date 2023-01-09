1b60ad178ffa9d1902cd75ed81630ade523e919a
Mateusz <mateusz.lichwiarz@gmail.com>
2023-01-09 02:18:00 +0100

    form character for user

---
+++ src/Form/Character/CreateCharacterFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form\Character;
+
+use App\Entity\Character;
+
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CreateCharacterFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('str', IntegerType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Character::class,
+        ]);
+    }
+}
